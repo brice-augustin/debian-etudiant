9da95570712b33b0508485f1fd6124c7b4e98ed1
stinaugu <stinaugu@macbook-air-de-stinaugu.home>
2018-06-20 23:04:00 +0200

    configuration du proxy

--- customize.sh
+++ customize.sh
@@ -11,6 +11,19 @@ echo vitrygtr | sudo -S apt-get update
 sudo apt-get install -y dnsutils
 
 ####
+# Proxy
+# P+VM
+####
+# Alternative : ajouter ça dans /etc/profile.d/proxy.sh
+echo "http_proxy=$PROXYIUT" >> /etc/bash.bashrc
+echo "https_proxy=$PROXYIUT" >> /etc/bash.bashrc
+echo "ftp_proxy=$PROXYIUT" >> /etc/bash.bashrc
+
+echo "Acquire::http::Proxy \"$PROXYIUT\";" > /etc/apt/apt.conf.d/80proxy
+
+exit
+
+####
 # Anciens noms de cartes réseau (eth0, pas ens33 ou enp0s3)
 ####
 
@@ -26,14 +39,3 @@ fi
 sed -E -i 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="net.ifnames=0 biosdevname=0"/' /etc/default/grub
 
 grub-mkconfig -o /boot/grub/grub.cfg
-
-####
-# Proxy
-# P+VM
-####
-# Alternative : ajouter ça dans /etc/profile.d/proxy.sh
-echo "http_proxy=$PROXYIUT" >> /etc/bash.bashrc
-echo "https_proxy=$PROXYIUT" >> /etc/bash.bashrc
-echo "ftp_proxy=$PROXYIUT" >> /etc/bash.bashrc
-
-echo "Acquire::http::Proxy \"$PROXYIUT\";" > /etc/apt/apt.conf.d/80proxy
