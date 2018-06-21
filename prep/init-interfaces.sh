92082355921acd9846ef1f56591fe0901427459e
stinaugu <stinaugu@MacBook-Air-de-stinaugu.local>
2018-06-21 23:26:15 +0200

    commentaire sur une methode pour savoir si un cable est branché

--- prep/init-interfaces.sh
+++ prep/init-interfaces.sh
@@ -5,6 +5,7 @@ echo "iface lo inet loopback" >> /etc/network/interfaces
 
 ethif=$(ip -o l show | awk -F': ' '{print $2}' | grep "^eth")
 
+# /sys/class/net/eth0/operstate (up ou down)
 for iface in $ethif
 do
   echo "auto $iface" >> /etc/network/interfaces
