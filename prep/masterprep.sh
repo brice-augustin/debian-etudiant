dd467cea9163cd6c0989a31cc1f5d2e3c10723da
Brice <brice.augustin@u-pec.fr>
2018-06-23 12:40:03 +0200

    unité systemd (pour initialiser le fichier interfaces) configurée directement dans le script

--- prep/masterprep.sh
+++ prep/masterprep.sh
@@ -21,10 +21,23 @@ fi
 
 ####
 # Configuration des interfaces au prochain reboot
+# P+VM
 ####
 # Copier script dans /etc/... qui s'exécute au boot et se supprime
 cp init-interfaces.sh /usr/local/bin
-cp init-interfaces.service /etc/systemd/system
+
+cat > /etc/systemd/system/init-interfaces.service << EOF
+[Unit]
+Description=Configuration du fichier interfaces au premier boot
+
+[Service]
+Type=oneshot
+RemainAfterExit=no
+ExecStart=/usr/local/bin/init-interfaces.sh
+
+[Install]
+WantedBy=multi-user.target
+EOF
 
 systemctl enable init-interfaces.service
 
@@ -32,4 +45,5 @@ systemctl enable init-interfaces.service
 # Empecher renommage des cartes réseau lors du clonage
 # P
 ####
+# N'existe plus sous stretch ?
 # /etc/udev/rules.d/70-persistent-net.rules
