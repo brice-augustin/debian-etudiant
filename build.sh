fad033dc26aa78a672baa6ed97b393a260bf4551
Brice <brice.augustin@u-pec.fr>
2019-06-13 17:13:43 +0200

    build VM with Packer (with or without the damn proxy)

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+s=http://cdimage.debian.org/cdimage/release/9.9.0/amd64/iso-cd/SHA512SUMS
+iso=http://cdimage.debian.org/cdimage/release/9.9.0/amd64/iso-cd/debian-9.9.0-amd64-netinst.iso
+
+PROXY=http://proxy.iutcv.fr:3128
+
+#http_proxy=$PROXY curl $s
+#http_proxy=$PROXY curl $iso
+# dl checksum et le passer en paramètre du json
+# idem iso
+
+cp preseed.cfg preseed-TMP.cfg
+
+if [ "$1" == "proxy" ]
+then
+  echo "d-i mirror/http/proxy string $PROXY" >> preseed-TMP.cfg
+fi
+
+packer validate debian.json
+packer build debian.json
